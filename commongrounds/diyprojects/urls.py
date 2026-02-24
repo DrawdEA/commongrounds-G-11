715f1c99d897dd4ecfc9ae906a67f61091701904
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2026-02-24 13:56:13 +0800

    Added initial templates for project list and detail, created class-based views for models.

--- commongrounds/diyprojects/urls.py
+++ commongrounds/diyprojects/urls.py
@@ -4,7 +4,9 @@ from .views import index
 
 urlpatterns = [
     path('', index, name='index'),
+    # TODO: Create urlpatterns for list and detail view
 ]
+
 
 
 app_name = "diyprojects"
--- commongrounds/diyprojects/views.py
+++ commongrounds/diyprojects/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Project
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
-
 
 def index(request):
     return HttpResponse('Hello World! This came from the index view')
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'diyprojects/project_list.html'
+
+class ProjectDetailView(DetailView):
+    model = Project 
+    template_name = 'diyproject/project_detail.html'
