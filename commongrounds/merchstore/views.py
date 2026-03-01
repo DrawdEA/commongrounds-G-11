888376112ae89fc33bda89c2e182ef4891d9ed6f
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2026-03-01 22:45:29 +0800

    Add urls, list and detail views, and templates

--- commongrounds/merchstore/views.py
+++ commongrounds/merchstore/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'merchstore/product_list.html'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'merchstore/product_detail.html'
