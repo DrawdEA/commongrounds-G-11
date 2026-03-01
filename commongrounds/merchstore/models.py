5b49bfbdbc655d6f083dfc50ba424c62fd6f7bf4
Edward Joshua Diesta <99942301+DrawdEA@users.noreply.github.com>
2026-03-01 22:28:38 +0800

    Migrate model additions

--- commongrounds/merchstore/models.py
+++ commongrounds/merchstore/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    product_type = models.ForeignKey(
+        ProductType,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='products'
+    )
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        if self.product_type:
+            return f"{self.name} ({self.product_type.name})"
+        return self.name
+
+    class Meta:
+        ordering = ['name']
