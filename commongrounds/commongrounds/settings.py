000ae5310114557625556366b698f40f2d4fbd23
Aldrin <nellasaldrin@gmail.com>
2026-03-06 02:45:41 +0800

    included bookclub to apps and added bookclub/ url

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookclub'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'commongrounds.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
