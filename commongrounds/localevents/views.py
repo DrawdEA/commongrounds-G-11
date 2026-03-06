45053be52d87d6d61f2a7a2ac555427353cbf075
Gio <@Gio>
2026-03-06 21:20:03 +0800

    updated view functions and contexts

--- commongrounds/localevents/views.py
+++ commongrounds/localevents/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 from .models import Event
 
 def event_list(request):
+    events = Event.objects.all()
+    ctx = {
+    'events': events
+    }
+    return render(request, 'localevents/event_list.html', ctx)
+
 def event_detail(request, pk):
+    event = Event.objects.get(id=pk)
+    ctx = { "event" : event}
+
+    return render(request, 'localevents/event_detail.html', ctx)
+
