eb698894cbc539940e6f949feb092ce44d6804e6
Gio <@Gio>
2026-03-06 21:19:04 +0800

    created models and appropriate fields

--- commongrounds/localevents/models.py
+++ commongrounds/localevents/models.py
@@ -4,4 +4,19 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 class EventType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
 class Event(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        EventType, 
+        on_delete = models.SET_NULL,
+        null = True,
+        related_name='categories',
+    )
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    start_time = models.DateTimeField(null=False)
+    end_time = models.DateTimeField(null=False)
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True,  null=True)
