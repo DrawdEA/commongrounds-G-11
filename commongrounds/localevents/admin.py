80d1663483a0bf642d60963e99186127a8082a54
Gio <@Gio>
2026-03-06 21:22:28 +0800

    created admin panel

--- commongrounds/localevents/admin.py
+++ commongrounds/localevents/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Event, EventType
+
 class EventAdmin(admin.ModelAdmin):
+    model = Event
+
 class EventTypeAdmin(admin.ModelAdmin):
+    model = EventType
+    
+admin.site.register(EventType, EventTypeAdmin)
+admin.site.register(Event, EventAdmin)
