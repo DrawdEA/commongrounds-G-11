76b5480c304b2a566a15d253cc732369ffd48730
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2026-04-01 09:14:22 +0800

    Added profile models to admin

--- commongrounds/accounts/views.py
+++ commongrounds/accounts/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World! This came from the index view')
+    return HttpResponse('Hello World! This came from the index view of the accounts app')
