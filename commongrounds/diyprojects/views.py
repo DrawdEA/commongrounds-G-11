39dcc2645e7b5124bb49bfdaa831918e4e65dd37
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2026-04-02 10:54:27 +0800

    feat: when user is logged in, they can see the projects they've interacted with.

--- commongrounds/diyprojects/views.py
+++ commongrounds/diyprojects/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
-from .models import Project
+from django.http import HttpResponse
+from .models import Project, Profile, Favorite, ProjectReview
 # Create your views here.
 
 
 def project_list(request):
     projects = Project.objects.all()
-    return render(request, 'diyprojects/project_list.html',
-                  {'project_list':projects})
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user=request.user)
+        created_projects = Project.objects.filter(creator=profile)
+        reviewed_projects = Project.objects.filter(project_reviews__reviewer=profile)
+        favorited_projects = Project.objects.filter(favorites__profile=profile)
+
+        # https://docs.djangoproject.com/en/dev/ref/models/querysets/#in
+        user_interacted_project_ids = (created_projects | reviewed_projects | favorited_projects).distinct().values_list('id')
+        other_projects = Project.objects.exclude(id__in=user_interacted_project_ids)
+
+        return render(request, 'diyprojects/project_list.html',
+                    {'other_projects':other_projects,
+                     'created_projects': created_projects,
+                     'reviewed_projects': reviewed_projects,
+                     'favorited_projects': favorited_projects})
+    else:
+        return render(request, 'diyprojects/project_list.html',
+                    {'project_list':projects})
 
 
 def project_detail(request, pk):
