ac2e7b307c845041b3f25c9af5c5fdc2ab868baa
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2026-04-06 08:40:11 +0800

    Added ROLES list to Profile model

--- commongrounds/accounts/models.py
+++ commongrounds/accounts/models.py
@@ -3,6 +3,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
+    ROLES = ["Market Seller",
+             "Event Organizer",
+             "Book Contributor",
+             "Project Creator",
+             "Commission Maker"]
+    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     display_name = models.CharField(max_length=63)
     email_address = models.EmailField()
