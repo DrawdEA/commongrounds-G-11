d248582a7d69b58ac24243f1ea9e0800f7f5358e
Gio <@Gio>
2026-04-17 16:54:46 +0800

    added variables to eventtype and added eventsignup

--- commongrounds/localevents/models.py
+++ commongrounds/localevents/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.core.validators import MinValueValidator
 
 
 class EventType(models.Model):
@@ -19,12 +20,20 @@ class Event(models.Model):
         EventType,
         on_delete=models.SET_NULL,
         null=True,
-        related_name='categories',
+        related_name='events',
     )
+    organizer = models.ManyToManyField(Profile)
+    event_image = models.ImageField()
     description = models.TextField()
     location = models.CharField(max_length=255)
     start_time = models.DateTimeField(null=False)
     end_time = models.DateTimeField(null=False)
+    event_capacity = models.PositiveIntegerField(
+        validators=[MinValueValidator(1)]
+    )
+    status = models.CharField(
+        choices = [('AVAIL', 'Available'), ('FULL', 'Full'), ('DONE', 'Done'), ('CANCEL', 'Cancelled')]
+    )
     created_on = models.DateTimeField(auto_now_add=True, null=True)
     updated_on = models.DateTimeField(auto_now=True,  null=True)
 
@@ -36,3 +45,23 @@ class Event(models.Model):
 
     class Meta:
         ordering = ['-created_on']
+
+
+class EventSignup():
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='signups',
+    )
+    user_registrant = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True;
+        related_name='signup_user'
+    )
+    new_registrant = models.CharField(
+        null=True,
+        blank=True
+    )
