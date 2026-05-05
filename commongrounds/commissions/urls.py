50b8f7e62f3656ca256bf3270bb24e57239dab14
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2026-05-05 11:51:10 +0800

    Added slash to commissions urls. Removed faulty default for bookclub models

--- commongrounds/commissions/urls.py
+++ commongrounds/commissions/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "commissions"
 
 urlpatterns = [
-    path("requests", views.commission_list, name="commission_list"),
-    path("request/add", views.commission_create, name="commission_create"), # <-- THIS LINE!
-    path("request/<int:pk>", views.commission_detail, name="commission_detail"),
-    path("request/<int:pk>/edit", views.commission_update, name="commission_update"),
+    path("requests/", views.commission_list, name="commission_list"),
+    path("request/add/", views.commission_create, name="commission_create"),
+    path("request/<int:pk>/", views.commission_detail, name="commission_detail"),
+    path("request/<int:pk>/edit/", views.commission_update, name="commission_update"),
 ]
