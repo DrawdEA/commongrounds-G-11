ffbf129c0abddb8378fae54f3787bb3c7a12e391
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2026-05-05 08:50:15 +0800

    Edited bookclub models.py to use the Profile model from accounts.py instead of its own

--- commongrounds/bookclub/models.py
+++ commongrounds/bookclub/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.username
-
+from accounts.models import Profile
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
