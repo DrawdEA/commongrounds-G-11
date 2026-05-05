3b587f4c164a80168f10ae3be905cd2c02e0b4b7
Gabriel Matthew Labariento <gabrielmatthew.labariento@gmail.com>
2026-05-05 13:06:53 +0800

    Updated instances of 'commission_maker' to 'Commission Maker'

--- commongrounds/commissions/views.py
+++ commongrounds/commissions/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Commission, Job, JobApplication  
-from .forms import CommissionForm, JobFormSet
+from .forms import CommissionForm, JobFormSet, JobApplicationForm
 from django.db.models import Case, When, Value, IntegerField
 # Create your views here.
 
@@ -52,18 +52,21 @@ def commission_detail(request, pk):
 
     open_manpower = total_manpower - accepted_signees
 
+    job_application_form = JobApplicationForm()
     if request.method == "POST" and request.user.is_authenticated:
         if request.user.profile == commission.maker:
             return redirect('commissions:commission_detail', pk=commission.pk)
 
-        job_id = request.POST.get('job_id')
-        job_to_apply = get_object_or_404(Job, pk=job_id)
+        job_application_form = JobApplicationForm(request.POST)
+        if job_application_form.is_valid():
+            job_id = request.POST.get('job_id')
+            job_to_apply = get_object_or_404(Job, pk=job_id)
 
-        JobApplication.objects.get_or_create(
-            job=job_to_apply,
-            applicant=request.user.profile,
-            defaults={'status': 'Pending'}
-        )
+            JobApplication.objects.get_or_create(
+                job=job_to_apply,
+                applicant=request.user.profile,
+                defaults={'status': 'Pending'}
+            )
         return redirect('commissions:commission_detail', pk=commission.pk)
 
     context = {
@@ -71,13 +74,14 @@ def commission_detail(request, pk):
         'total_manpower': total_manpower,
         'open_manpower': open_manpower,
         'jobs': jobs,
+        'job_application_form': job_application_form,
     }
     return render(request, 'commissions/commission_detail.html', context)
 
 
 @login_required
 def commission_create(request):
-    if request.user.profile.role != "commission_maker":
+    if request.user.profile.role != "Commission Maker":
         return redirect('commissions:commission_list')
 
     if request.method == "POST":
@@ -108,7 +112,7 @@ def commission_create(request):
 def commission_update(request, pk):
     commission = get_object_or_404(Commission, pk=pk)
 
-    if request.user.profile.role != "commission_maker" or request.user.profile != commission.maker:
+    if request.user.profile.role != "Commission Maker" or request.user.profile != commission.maker:
         return redirect('commissions:commission_list')
 
     if request.method == "POST":
